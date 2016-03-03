089851ebbe7e45fd9b86bacddfd82aca150033e3
Nikita <ageevnikitatest@gmail.com>
2016-03-03 17:27:50 +0200

    NumberThree

--- pdffiller/src/main/java/courseJava/NumberThree.java
+++ pdffiller/src/main/java/courseJava/NumberThree.java
@@ -2,7 +2,6 @@ package courseJava;
 
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -14,38 +13,49 @@ public class NumberThree {
 
     public static void main(String[] args) {
 
+    List<Integer> list1 = new ArrayList();
+    List<Integer> list2 = new ArrayList();
+    List<Integer> numbers = new ArrayList<>();
+    int a = 0;
+    int array[] = {2, 4, 6, 8, 10, 22, 34, 40, -5, 12, 14, 46, 46, 54, 38, 92};
+ 
 
-        List<Integer> list1 = new ArrayList();
-        List<Integer> list2 = new ArrayList();
-        List<Integer> numbers = new ArrayList<>();
-        int array[] = {2, 4, 6, 8, 10, 22, 34, 40, -5, 12, 14, 46, 46, 54, 38, 92};
 
         for (int i = 0; i < array.length; i++) {
+
             numbers.add(array[i]);
         }
-        int a= 0;
+        System.out.println(numbers);
+
+        for (int i = 0; i < numbers.size(); i++) {   //вытягивает из листа поочередно все значения
 
 
-
-        for (int i = 0; i < numbers.size(); i++) {     // вытягивает из листа поочередно все значения
-
-            if (numbers.get(Integer.valueOf(i)) > 0) {
-
-
-                                                            // берем значения которые больше 0 (положительные)
+            if (numbers.get(Integer.valueOf(i)) > 0) {            // берем значения которые больше 0 (положительные)
                 list1.add(numbers.get(Integer.valueOf(i)));
 
+            }
 
+            else{
+                a=i;
+                for (  ; i < numbers.size(); i++){
+                    list2.add(numbers.get(Integer.valueOf(i)));
+                }
+
+                break;
             }
-             else {
-                a = i ;
-                break;}
-            }
+        }
 
         numbers.remove(a);
+        list2.remove(0);
 
-            System.out.println(numbers);
+        System.out.println(list1);
+        System.out.println(list2);
+
+
         }
     }
+
+
+
 
 
