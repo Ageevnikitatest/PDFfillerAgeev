0c02bb92caf44cdb5cbef5d3c142c72ea6ac0236
Nikita <ageevnikitatest@gmail.com>
2016-05-17 16:48:37 +0300

    Api response

---
+++ pdffiller/src/main/java/courseJava/api/JsonStandart.java
@@ -0,0 +1,21 @@
+package courseJava.api;
+
+/**
+ * Created by ANS on 28/04/16.
+ */
+public class JsonStandart {
+
+    public static void json(int formId){
+
+        String jsonReq = "{\n" +
+                "\"formId\":\"30\",\n" +
+                "\"pdfUrl\":\"http://www.ada.gov/lsac_best_practices_report.pdf\",\n" +
+                "\"pdfVersion\":1,\n" +
+                "\"sourceFormat\":\"pdf\",\n" +
+                "\"options\":{\"imageFormat\":\"png\"},\n" +
+                "\"levels\":\"1000\",\n" +
+                "\"sync\":\"true\"}";
+
+    }
+
+}
--- pdffiller/src/main/java/courseJava/api/request1.java
+++ pdffiller/src/main/java/courseJava/api/request1.java
@@ -14,7 +14,7 @@ import java.io.IOException;
  */
 public class Request1 {
 
-    public static String host = "www.pdffiller.com";
+    public static String host = "www.irs.gov/pub/irs-pdf/fw2.pdf";
     public static String scheme = "https"; // указать схему
     public static String appKey ="web_fds3w43342Sbs";  // доп ключ, откуда обращаемся
     public static String device = "web";
@@ -24,13 +24,13 @@ public class Request1 {
     public static String token;
 
     public URIBuilder uriBuilder =  new URIBuilder().setScheme(scheme).setHost(host).setParameter("device", device).setParameter("appKey",appKey);
-
+    // uriBuilder собирает из всех параметров один запрос
 
     public String getResponseBody(HttpResponse resp) throws ParseException, IOException {
         String respBody = EntityUtils.toString(resp.getEntity());
         int statusCode = resp.getStatusLine().getStatusCode();
-        System.out.println("RESPONSE: "+respBody);
-        Assert.assertEquals(statusCode, HttpStatus.SC_OK, "Status code: "+statusCode);
+        System.out.println("RESPONSE: " + respBody);
+        Assert.assertEquals(statusCode, HttpStatus.SC_OK, "Status code: " + statusCode);
         return respBody;
     }
 
